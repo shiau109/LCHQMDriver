58419cfa782ab3a4455df8374006be003ddc50bb
shiau109 <shiau109@gmail.com>
2025-08-13 15:33:26 +0800

    add temporal steering

---
+++ calibration_utils/LCH_temporal_steering/parameters.py
@@ -0,0 +1,24 @@
+from qualibrate import NodeParameters
+from qualibrate.parameters import RunnableParameters
+from qualibration_libs.parameters import QubitsExperimentNodeParameters, CommonNodeParameters, IdleTimeNodeParameters
+from customized.common_parameters import CommonFluxParameters
+from typing import Optional
+
+
+class NodeSpecificParameters(RunnableParameters):
+    num_shots: int = 1000
+    """Number of averages to perform. Default is 1000."""
+    prepare_gate: Optional[str] = None
+    """Prepare gate to use for the qubit. Default is "X90"."""
+    readout_basis: Optional[str] = None
+    """Tomography gate to use for the qubit. Default is "None" and "Z"."""
+
+class Parameters(
+    NodeParameters,
+    CommonNodeParameters,
+    IdleTimeNodeParameters,
+    NodeSpecificParameters,
+    QubitsExperimentNodeParameters,
+    CommonFluxParameters,
+):
+    pass
