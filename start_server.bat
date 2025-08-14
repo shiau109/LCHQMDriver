8f9aa585eb8b4e9c8a1ab13193fa2e834e052000
shiau109 <shiau109@gmail.com>
2025-08-14 23:38:25 +0800

    Create start_server.bat

---
+++ start_server.bat
@@ -0,0 +1,19 @@
+@echo off
+REM Change directory to the one where the batch file was launched
+cd /d %~dp0
+
+REM Define variables
+set "ENV_NAME=ASQMDriver"
+set "ACTIVATE_PATH=%USERPROFILE%\miniconda3\Scripts\activate.bat"
+REM anaconda3 or miniconda3
+
+REM Open Anaconda Prompt, activate the environment, and run the command
+echo Open conda environment from "%ACTIVATE_PATH%"
+CALL "%ACTIVATE_PATH%" %ENV_NAME%
+
+REM start qualibrate server command
+
+qualibrate start
+
+REM Keep the window open so you can see any output
+pause
