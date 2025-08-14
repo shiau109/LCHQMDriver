442d01b7e5b685546c169e39ca37b6bb4dd982c8
shiau109 <shiau109@gmail.com>
2025-08-14 23:38:53 +0800

    Test ignore

--- quam_config/my_quam.py
+++ quam_config/my_quam.py
@@ -1,7 +1,20 @@
 from quam_builder.architecture.superconducting.qpu import FixedFrequencyQuam, FluxTunableQuam
 
+from quam_builder.architecture.superconducting.qubit_pair import FluxTunableTransmonPair
+from typing import Dict, Union, ClassVar, Type
+from dataclasses import field
+
+
+
+from quam_builder.architecture.superconducting.components.tunable_coupler import (
+    TunableCoupler,
+)
+
+
+# class LCH_TunableCoupler(TunableCoupler):
 
 # Define the QUAM class that will be used in all calibration nodes
 # Should inherit from either FixedFrequencyQuam or FluxTunableQuam
 class Quam(FluxTunableQuam):
-    pass
+    qubit_pair_type: ClassVar[Type[FluxTunableTransmonPair]] = FluxTunableTransmonPair
+    qubit_pairs: Dict[str, FluxTunableTransmonPair] = field(default_factory=dict)
